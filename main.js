5bc24b6143f514ae79bf23ee7ed7773bccf9067c
kkkk2058 <kkkk2058@naver.com>
2022-06-26 14:25:31 +0000

    JS:  RFINNAL CODE

--- main.js
+++ main.js
@@ -22,8 +22,9 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-	navbarMenu.classList.remove('open');
-	scrollIntoView(link);
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+	
 });
 
 
@@ -110,11 +111,79 @@ workBtnContainer.addEventListener('click', (e) => {
 
 
 
+		
+
+	const sectionIds = ['#home','#about','#skills','#work','#testimonials','#contact'];
+	const sections = sectionIds.map(id => document.querySelector(id));
+	const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+	
+	
+	let selectedNavIndex=0;
+	let selectedNavItem=navItems[0];
+	
+	function selectNavItem(selected){
+				selectedNavItem.classList.remove('active');
+				selectedNavItem =selected;
+				selectedNavItem.classList.add('active');
+	}
+	
 
 
-function scrollIntoView(selector) {
+	function scrollIntoView(selector) {
 	const scrollTo = document.querySelector(selector);
 	scrollTo.scrollIntoView({ behavior: 'smooth' });
+	selectNavItem(navItems[sectionIds.indexOf(selector)]);
+		
 }
+		
 
 
+
+	const observerOption={
+	root:null,
+	rootMargin:'0px',
+	threshold:0.3,
+};
+
+
+
+
+		const observerCallback =(entries, observer) => {
+			 entries.forEach(entry => {
+			if(!entry.isIntersecting && entry.intersectionRatio>0){
+				const index=sectionIds.indexOf(`#${entry.target.id}`)
+
+				if(entry.boundingClientRect.y<0){
+					selectedNavIndex=index+1;
+				}
+				else{
+					selectedNavIndex=index-1;
+				}
+			}
+				
+
+			});
+		};
+
+
+		
+
+		const observer = new IntersectionObserver(observerCallback,observerOption);
+		
+		sections.forEach(section => observer.observe(section));
+		
+
+	
+		window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    window.scrollY + window.innerHeight ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+	
